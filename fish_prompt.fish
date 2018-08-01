dd1e81e3b621a6e81b156a8f7c144dc4b9df26e8
Combro2k <726584+combro2k@users.noreply.github.com>
2018-08-01 12:25:58 +0200

    Update fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -19,7 +19,7 @@ end
 ## Function to show current status
 function show_status -d "Function to show the current status"
   if [ $RETVAL -ne 0 ]
-    prompt_segment red black "($RETVAL) ▲ "
+    prompt_segment red black " ▲ $RETVAL "
     set pad ""
     end
   if [ -n "$SSH_CLIENT" ]
